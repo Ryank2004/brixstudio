4f8d466413ebbf46ee3a6a22155cb3e305d45933
Ryank2004 <ryankorver04@gmail.com>
2025-03-20 16:49:41 +0100

    logo rm test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import path from 'path';
 
-export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        '@atoms': path.resolve('./src/components/atoms'),
-        '@molecules': path.resolve('./src/components/molecules'),
-        '@organisms': path.resolve('./src/components/organisms')
-      }
-    }
-  }
-});
+// https://astro.build/config
+export default defineConfig({});
