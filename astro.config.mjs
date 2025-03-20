b3e9483a92d1579793ea337de2f349ad522397aa
Ryank2004 <ryankorver04@gmail.com>
2025-03-20 16:43:11 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import alias from '@rollup/plugin-alias';
+import path from 'path';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      alias({
+        entries: [
+          { find: '@atoms', replacement: path.resolve(__dirname, 'src/components/atoms') },
+          { find: '@molecules', replacement: path.resolve(__dirname, 'src/components/molecules') },
+          { find: '@organisms', replacement: path.resolve(__dirname, 'src/components/organisms') }
+        ]
+      })
+    ]
+  }
+});
+
